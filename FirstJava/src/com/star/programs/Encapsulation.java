4b3cb5bb7996c76e139e8adab62ffa3586858e7e
kt786 <kt786@Talha_laptop>
2024-06-19 18:29:39 +0530

    Initial commit

---
+++ FirstJava/src/com/star/programs/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.star.programs;
+
+public class Encapsulation {
+
+	int age;
+	int ser_num;
+	String name;
+	
+	public void show(){
+		System.out.print("Seriel Number:"+ser_num+"\t Name:"+name+"\t age:"+age);
+	}
+}
---
+++ FirstJava/src/com/star/programs/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.star.programs;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation obj =new Encapsulation();
+		obj.ser_num=22;
+		obj.name="Alex";
+		obj.age=20;
+		obj.show();
+	}
+
+}
