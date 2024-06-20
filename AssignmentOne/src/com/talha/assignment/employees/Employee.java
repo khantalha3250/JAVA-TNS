d3fad78767a1cc4bd2482995dd4a9743903cebe4
kt786 <kt786@Talha_laptop>
2024-06-20 20:35:50 +0530

    Doubt

---
+++ AssignmentOne/src/com/talha/assignment/employees/Employee.java
@@ -0,0 +1,25 @@
+package com.talha.assignment.employees;
+
+public class Employee {
+	private String name;
+	private int salary;
+	private int employeeid;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getEmployeeid() {
+		return employeeid;
+	}
+	public void setEmployeeid(int employeeid) {
+		this.employeeid = employeeid;
+	}
+}
---
+++ AssignmentOne/src/com/talha/assignment/employees/Manager.java
@@ -0,0 +1,5 @@
+package com.talha.assignment.employees;
+
+public class Manager {
+	
+}
